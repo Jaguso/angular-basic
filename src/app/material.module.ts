c68968df95cef1642835509c419dc9d6e087e080
Jaguso <jagutzo@gmail.com>
2019-01-29 18:23:08 -0600

    one form made

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core'
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 const modules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatInputModule,
+  MatButtonModule
 ]
 
 @NgModule({
--- src/app/one-form/one-form.component.ts
+++ src/app/one-form/one-form.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'one-form',
   templateUrl: './one-form.component.html',
   styleUrls: ['./one-form.component.css']
 })
-export class OneFormComponent { }
+export class OneFormComponent {
+  onSubmit(form: NgForm) {
+    console.log(form.value.description);
+  }
+}
